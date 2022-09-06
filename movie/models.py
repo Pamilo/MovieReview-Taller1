342ccf967ed90379933a545ba90b17af79b4beed
Pamilo <81716232+Pamilo@users.noreply.github.com>
2022-09-06 10:24:27 -0500

    Visualizacion de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Movie (models.Model):
+    Title=models.CharField(max_length=100)
+    Descripcion=models.CharField(max_length=250)
+    image= models.ImageField(upload_to='movie/images/')
+    url=models.URLField(blank=True)
 # Create your models here.
--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request,'home.html')
-    return render(request,'home.html',{'name':'Pablo Micolta Lopez'})
+    #return render(request,'home.html',{'name':'Pablo Micolta Lopez'})
+    searchTerm= request.GET.get('searchMovie')
+    if searchTerm:
+        movies=Movie.objects.filter(Title__icontains=searchTerm)
+    else:
+        movies=Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
